4dfaaece39714177344f356a5a2a396ca12a6227
avanie-s <avaniesharma1806@gmail.com>
2025-10-18 20:05:32 +0530

    adding features

--- project.py
+++ project.py
@@ -1,3 +1,10 @@
 a = int(input("enter your age"))
 b = input("enter you name")
 print("hello", b, "how does it feel like to be", a, "?")
+student = {
+
+    "name" : "Ava",
+    "age"  : 18,
+}
+
+print(student.get["name"])
